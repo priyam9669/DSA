bd83a6f4460486a296b496ab8e8ae5fd0cdbbe1b
priyam9669 <priyamkumardas@gmail.com>
2023-08-21 21:25:28 +0530

    reverse link list

--- src/Bolier.java
+++ src/Bolier.java
@@ -4,5 +4,32 @@ public class Bolier {
 
     }
 
+
+    public ListNode reverseList(ListNode head) {
+        ListNode tempnext;
+        ListNode prev=null;
+        ListNode current= head;
+        while( current!=null){
+            tempnext= current.next;
+            current.next=prev;
+            prev=current;
+            current=tempnext;
+        }
+        return prev;
+    }
+
     
 }
+
+
+
+ // Definition for singly-linked list.
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+
